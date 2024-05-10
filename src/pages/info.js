c495b0d0cb697ff30903873335db68421e7dfb2e
Mohamed <mouss.mo.ke@gmail.com>
2024-05-10 21:42:20 +0200

    unique header background

--- src/pages/info.js
+++ src/pages/info.js
@@ -8,29 +8,7 @@ export default function Info() {
   return (
     <Layout page="info">
       <section className={infoStyles.info_blurb}>
-        <h2>
-          <div dangerouslySetInnerHTML={{__html: infoData.description}}></div>
-          <div dangerouslySetInnerHTML={{__html: infoData.cta}}></div>
-        </h2>
-        <ul>
-          <li>
-            <p>
-              <a href={`mailto:${infoData.contact.email}`}>Email: {infoData.contact.email}</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://twitter.com/${infoData.contact.twitter_handle}`}>
-                Twitter: @{infoData.contact.twitter_handle}
-              </a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://github.com/${infoData.contact.github_handle}`}>Github: {infoData.contact.github_handle}</a>
-            </p>
-          </li>
-        </ul>
+
       </section>
     </Layout>
   )
