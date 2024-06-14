ad9b27dbd081d007d934b5ceaa0ba544b8c6c272
Mohamed <mouss.mo.ke@gmail.com>
2024-06-15 00:09:24 +0200

    about me logos and image

--- src/pages/info.js
+++ src/pages/info.js
@@ -2,12 +2,17 @@ import React from "react"
 import Layout from "../components/Layout"
 import * as infoStyles from "../styles/pages/info.module.scss"
 import useSiteMetaData from "../static_queries/useSiteMetadata"
+import githubLogoImage from '../../static/github-logo.png'
+import linkedinLogoImage from '../../static/linkedin-logo.png'
+import aboutmeImage from '../../static/aboutmePicture.jpg'
+
 
 export default function Info() {
   const { infoData } = useSiteMetaData()
   return (
     <Layout page="info">
         <div className={infoStyles.responsiveContainerBlock}>
+        <img className={infoStyles.profilePicture} src={aboutmeImage}/>
           <p className={infoStyles.textBlk + ' ' + infoStyles.heading}>
             About Me
           </p>
@@ -15,22 +20,15 @@ export default function Info() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
           </p>
           <div className={infoStyles.socialIconsContainer}>
-            <a className={infoStyles.socialIcona}>
-              <img className={infoStyles.socialIcon + ' ' + infoStyles.imageBlock} 
-              src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb33.png"/>
+            <a className={infoStyles.socialIcona} href="https://github.com/moussmo" target="_blank">
+              <img className={infoStyles.socialIcon} 
+              src={githubLogoImage}/>
             </a>
-            <a className={infoStyles.socialIcona}>
-              <img className={infoStyles.socialIcon + ' ' + infoStyles.imageBlock}
-               src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb34.png"/>
+            <a className={infoStyles.socialIcona} href="https://www.linkedin.com/in/mo-mouss/" target="_blank">
+              <img className={infoStyles.socialIcon}
+               src={linkedinLogoImage}/>
             </a>
-            <a className={infoStyles.socialIcona}>
-              <img className={infoStyles.socialIcon + ' ' + infoStyles.imageBlock}
-               src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png"/>
-            </a>
-            <a className={infoStyles.socialIcona}>
-              <img className={infoStyles.socialIcon + ' ' + infoStyles.imageBlock} 
-              src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb36.png"/>
-            </a>
+
           </div>
         </div>
     </Layout>
