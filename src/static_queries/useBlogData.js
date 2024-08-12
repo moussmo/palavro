9cd44fceb5a3f87c7d1ad35c8670bf7df2c389ff
Mohamed <mouss.mo.ke@gmail.com>
2024-08-13 01:01:46 +0200

    no more flashy hover

--- src/static_queries/useBlogData.js
+++ src/static_queries/useBlogData.js
@@ -17,11 +17,6 @@ export default function useBlogData() {
                 gatsbyImageData
               }
             }
-            thumbnail_image {
-              childImageSharp {
-                gatsbyImageData
-              }
-            }
           }
           excerpt (pruneLength: 160)
         }
