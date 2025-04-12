11afcb4d29320f55080b3b4f0515d1dd995c840f
Mohamed <mouss.mo.ke@gmail.com>
2025-04-12 14:09:04 +0200

    trying out new blog list

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -3,34 +3,53 @@ import { Link } from "gatsby"
 import useBlogData from "../static_queries/useBlogData"
 import * as blogListStyles from "../styles/components/bloglist.module.scss"
 import MajorArticleCard from "./MajorArticleCard"
+import MinorArticleCard from "./MinorArticleCard"   
 
 export default function BlogList({type}) {
   const blogData = useBlogData()
   function renderBlogData() {
+    const filteredData = blogData
+      .filter(blog => blog.node.frontmatter.title !== "")
+      .filter(blog => {
+        if (type != undefined) {
+          const article_type = blog.node.fileAbsolutePath.split("/").reverse()[1]
+          return type === article_type
+        }
+        return true
+      })
+  
+    const mainArticle = filteredData[0]
+    const secondaryArticles = filteredData.slice(1, 3)
+    const minorArticles = filteredData.slice(3)
+  
     return (
       <div className={blogListStyles.list}>
-        {blogData
-          .filter(blog => blog.node.frontmatter.title !== "")
-          .filter(blog => {
-            if (type!=undefined){
-              const article_type = blog.node.fileAbsolutePath.split('/').reverse()[1]
-              return type == article_type
-            }
-            else {
-              return true
-            }
-          })
-          .map(blog => {
-            return (
-              <MajorArticleCard blog={blog}/>
-            )
-          })}
+        {/* All MajorArticleCards grouped together */}
+        <div className={blogListStyles.majorCards}>
+          {/* First major card */}
+          {mainArticle && <MajorArticleCard blog={mainArticle} />}
+  
+          {/* Secondary cards in a nested div */}
+          <div className={blogListStyles.secondaryCards}>
+            {secondaryArticles.map((blog) => (
+              <MajorArticleCard key={blog.node.id} blog={blog} secondary />
+            ))}
+          </div>
+        </div>
+  
+        {/* Minor cards */}
+        <div className={blogListStyles.minorCards}>
+          {minorArticles.map((blog) => (
+            <MinorArticleCard key={blog.node.id} blog={blog} />
+          ))}
+        </div>
       </div>
     )
   }
+  
   return (
     <section>
-      <ul >{renderBlogData()}</ul>
+      <ul className={blogListStyles.ul}>{renderBlogData()}</ul>
     </section>
   )
 }
--- src/components/MajorArticleCard.js
+++ src/components/MajorArticleCard.js
@@ -1,27 +1,39 @@
 import React from "react"
 import { Link } from "gatsby"
 import * as MajorArticleCardStyles from "../styles/components/majorarticlecard.module.scss"
-import {GatsbyImage, getImage} from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-export default function MajorArticleCard({blog}) {
-    const image = getImage(blog.node.frontmatter.hero_image.childImageSharp.gatsbyImageData)
-    return (<div>
-                <Link to={`/blog/${blog.node.frontmatter.slug}`} key={blog.node.id}>
-                    <li className={MajorArticleCardStyles.articleCard} key={blog.node.frontmatter.slug}>
-                        <GatsbyImage className={MajorArticleCardStyles.articleHero} image={image} loading="eager" alt="article_thumbnail" />
-                        <div className={MajorArticleCardStyles.articleBlackMask}/>
-                        <div className={MajorArticleCardStyles.articleBlurMask}/>
-                        <div className={MajorArticleCardStyles.articleOverlay}>
-                        <div className={MajorArticleCardStyles.articleMeta}>
-                            <span className={MajorArticleCardStyles.articleCategory}>SCIENCE</span>
-                            <div className={MajorArticleCardStyles.articleDate}>{blog.node.frontmatter.date}</div>
-                        </div>
-                        <div className={MajorArticleCardStyles.articleTitle}>{blog.node.frontmatter.title}</div>
-                        <p className={MajorArticleCardStyles.articleExcerpt}>{blog.node.excerpt}</p>
-                        </div>
-                    </li>
-                </Link>
+export default function MajorArticleCard({ blog, secondary }) {
+  const image = getImage(blog.node.frontmatter.hero_image.childImageSharp.gatsbyImageData)
+  const getClass = (baseClass) => 
+    `${baseClass} ${secondary ? MajorArticleCardStyles.secondary : ""}`.trim()
+
+  return (
+        <li className={getClass(MajorArticleCardStyles.articleCard)} key={blog.node.frontmatter.slug}>
+        <Link to={`/blog/${blog.node.frontmatter.slug}`} key={blog.node.id}>
+          <GatsbyImage
+            className={getClass(MajorArticleCardStyles.articleHero)}
+            image={image}
+            loading="eager"
+            alt="article_thumbnail"
+          />
+          <div className={getClass(MajorArticleCardStyles.articleBlackMask)} />
+          <div className={getClass(MajorArticleCardStyles.articleBlurMask)} />
+          <div className={getClass(MajorArticleCardStyles.articleOverlay)}>
+            <div className={getClass(MajorArticleCardStyles.articleMeta)}>
+              <span className={getClass(MajorArticleCardStyles.articleCategory)}>SCIENCE</span>
+              <div className={getClass(MajorArticleCardStyles.articleDate)}>
+                {blog.node.frontmatter.date}
+              </div>
             </div>
-            )
-        }
-
+            <div className={getClass(MajorArticleCardStyles.articleTitle)}>
+              {blog.node.frontmatter.title}
+            </div>
+            <p className={getClass(MajorArticleCardStyles.articleExcerpt)}>
+              {blog.node.excerpt}
+            </p>
+          </div>
+          </Link>
+        </li>
+  )
+}
