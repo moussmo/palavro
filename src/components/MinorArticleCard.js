9faef21719cb541062a5deef530a6e185a9085ef
Mohamed <mouss.mo.ke@gmail.com>
2025-05-10 12:46:45 +0200

    gotta fix those category boxes

--- src/components/MinorArticleCard.js
+++ src/components/MinorArticleCard.js
@@ -17,12 +17,14 @@ export default function MinorArticleCard({ blog }) {
             </Link>
             <Link to={`/blog/${blog.node.frontmatter.slug}`} key={blog.node.id}>
               <div className={MinorArticleCardStyles.articleData}>
+                <div className={MinorArticleCardStyles.articleHeader}>
                   <div className={MinorArticleCardStyles.articleMeta}>
                     <div className={MinorArticleCardStyles.articleDate}>{blog.node.frontmatter.date}</div>
                     <Link to={`/${blog.node.fileAbsolutePath.split("/").reverse()[1]}`} className={MinorArticleCardStyles.articleCategory}>{blog.node.fileAbsolutePath.split("/").reverse()[1]}</Link>
                   </div>
                   <div className={MinorArticleCardStyles.articleTitle}>{blog.node.frontmatter.title}</div>
-                  <div className={MinorArticleCardStyles.articleExcerpt}>{blog.node.excerpt}</div>
+                </div>
+                <div className={MinorArticleCardStyles.articleExcerpt}>{blog.node.excerpt}</div>
               </div>
               </Link>
         </div>
