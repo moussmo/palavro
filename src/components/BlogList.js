41646434cd6de920ceb21f0d5dd1ec0a287f39f7
Mohamed Moussaoui <mouss.mo.ke@gmail.com>
2025-05-11 16:48:02 +0200

    transition for minor article cards

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import useBlogData from "../static_queries/useBlogData"
 import * as blogListStyles from "../styles/components/bloglist.module.scss"
@@ -7,12 +7,22 @@ import MinorArticleCard from "./MinorArticleCard"
 
 export default function BlogList({ type }) {
   const blogData = useBlogData();
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useEffect(() => {
+    if (type !== undefined) {
+      const timer = setTimeout(() => {
+        setShowAnimation(true);
+      }, 100); // delay helps CSS transition work properly
+      return () => clearTimeout(timer);
+    }
+  }, [type]);
 
   function renderBlogData() {
     const filteredData = blogData
       .filter(blog => blog.node.frontmatter.title !== "")
       .filter(blog => {
-        if (type != undefined) {
+        if (type !== undefined) {
           const article_type = blog.node.fileAbsolutePath.split("/").reverse()[1];
           return type === article_type;
         }
@@ -20,19 +30,25 @@ export default function BlogList({ type }) {
       });
 
     if (type !== undefined) {
-      // Render all as MinorArticleCards
       return (
         <div className={blogListStyles.list}>
           <div className={blogListStyles.minorList}>
-            {filteredData.map(blog => (
-              <MinorArticleCard key={blog.node.id} blog={blog} />
+            {filteredData.map((blog, index) => (
+              <div
+                key={blog.node.id}
+                className={`${blogListStyles.articleBox} ${
+                  showAnimation ? blogListStyles.visible : ""
+                }`}
+                style={{ transitionDelay: `${index * 100}ms` }}
+              >
+                <MinorArticleCard blog={blog} />
+              </div>
             ))}
           </div>
         </div>
       );
     }
 
-    // Default behavior: headline, 3 major articles, rest minor
     const headline = filteredData[0];
     const majorarticles = filteredData.slice(1, 4);
     const minorarticles = filteredData.slice(4);
@@ -40,9 +56,7 @@ export default function BlogList({ type }) {
     return (
       <div className={blogListStyles.list}>
         <div className={blogListStyles.majorList}>
-          <div>
-            {headline && <MajorArticleCard blog={headline} />}
-          </div>
+          <div>{headline && <MajorArticleCard blog={headline} />}</div>
           {majorarticles.map(blog => (
             <MajorArticleCard key={blog.node.id} blog={blog} secondary />
           ))}
