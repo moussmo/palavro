e1c7460837e8a3974a10cc11911fbdff002c8acd
Mohamed <mouss.mo.ke@gmail.com>
2025-05-14 15:48:54 +0200

    responsiveness start

--- src/pages/history_art.js
+++ src/pages/history_art.js
@@ -1,8 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
 import BlogList from "../components/BlogList"
-import * as layoutStyles from "../styles/components/layout.module.scss"
-import {Link } from "gatsby"
 
 export default function IndexPage() {
   return (
